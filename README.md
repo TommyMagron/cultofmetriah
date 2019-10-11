cc9d18504d04a9640eb5478b6d84bbac96ac59ee
Tommy <tommy.magron@gmail.com>
2019-10-12 00:40:51 +0200

    feat (title): initialize screen title background image
    see above

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Cult Of Metriah
 
 Game project based on RGSS3 engine
+
+Use rvpacker to update rvdata2 files: https://github.com/Solistra/rvpacker
--- Scripts/Main.rb
+++ Scripts/Main.rb
@@ -4,4 +4,8 @@
 #  This processing is executed after module and class definition is finished.
 #==============================================================================
 
+# Proceed to resize in full screen mode
+Graphics.resize_screen(1920,1080)
+
 rgss_main { SceneManager.run }
+
